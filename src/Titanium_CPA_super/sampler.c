a553a2b4a975f2c51d55447fb48fd33a4c496657
Raymond K. Zhao <raymond.zhao@monash.edu>
2018-03-07 16:46:25 +1100

    Unparallelize some high overhead part of the code

--- src/Titanium_CPA_super/sampler.c
+++ src/Titanium_CPA_super/sampler.c
@@ -22,14 +22,10 @@
 #define B1_T (NUM_B1 / (K1 + 1)) /* the number of r_i exclusively sampling from B_1 */
 #define B1_REMAINING (NUM_B1 - B1_T * (K1 + 1)) /* the number of B_1 samples in r_t which has both B_1 and B_2 */
 
-static const __m256i V_B_1_BITMASK = {B_1_BITMASK, B_1_BITMASK, B_1_BITMASK, B_1_BITMASK};
-static const __m256i V_B_2_BITMASK = {B_2_BITMASK, B_2_BITMASK, B_2_BITMASK, B_2_BITMASK};
 static const __m256i V_1_1_1_1 = {1, 1, 1, 1};
 static const __m256i V_0_0_0_0 = {0, 0, 0, 0};
 static const __m256i V_fe_fe_fe_fe = {0xfffffffffffffffeLL, 0xfffffffffffffffeLL, 0xfffffffffffffffeLL, 0xfffffffffffffffeLL};
 static const __m256i V_Q_Q_Q_Q = {Q, Q, Q, Q};
-static const __m256i V_B_ADDMASK = {BINOMIAL_ADDMASK, BINOMIAL_ADDMASK, BINOMIAL_ADDMASK, BINOMIAL_ADDMASK};
-static const __m256i V_B_MASK = {BINOMIAL_MASK, BINOMIAL_MASK, BINOMIAL_MASK, BINOMIAL_MASK};
 static const __m256i V_0_1_2_3 = {0, 1, 2, 3};
 
 /* Sampler of r_i
@@ -41,56 +37,49 @@ void sampler_zb(uint64_t sample[T][N + K1 + 1])
 	uint32_t i;
 	uint32_t t;
 	unsigned char *rr;
-	__m256i y, x, z;
-	
+	__m256i y1, x1, z1;
+	uint32_t y;
+
 	fastrandombytes(r, B_BYTE * T * (K1 + 1) + T * (K1 + 1) / 8);
 	
 	/* sample B1_T r_i full of B_1 */
 	for (t = 0; t < B1_T; t++)
 	{
 		rr = r + B_BYTE * t * (K1 + 1);
-		for (i = 0; i < K1 + 1; i += 4)
+		for (i = 0; i < K1 + 1; i++)
 		{
-			y = _mm256_set_epi64x(LOAD_B(rr + (i + 3) * B_BYTE), LOAD_B(rr + (i + 2) * B_BYTE), LOAD_B(rr + (i + 1) * B_BYTE), LOAD_B(rr + i * B_BYTE));
-			y = _mm256_and_si256(y, V_B_1_BITMASK);
-			y = _mm256_add_epi64(y, V_1_1_1_1);
+			y = LOAD_B(rr + i * B_BYTE);
 			
-			_mm256_storeu_si256((__m256i *)(sample[t] + i), y);
+			sample[t][i] = (y & B_1_BITMASK) + 1;
 		}
 	}
 	
 	/* for r_t, sample the B_1 samples first */
 	rr = r + B_BYTE * B1_T * (K1 + 1);
-	for (i = 0; i < B1_REMAINING; i += 4)
+	for (i = 0; i < B1_REMAINING; i++)
 	{
-		y = _mm256_set_epi64x(LOAD_B(rr + (i + 3) * B_BYTE), LOAD_B(rr + (i + 2) * B_BYTE), LOAD_B(rr + (i + 1) * B_BYTE), LOAD_B(rr + i * B_BYTE));
-		y = _mm256_and_si256(y, V_B_1_BITMASK);
-		y = _mm256_add_epi64(y, V_1_1_1_1);
-			
-		_mm256_storeu_si256((__m256i *)(sample[B1_T] + i), y);
+		y = LOAD_B(rr + i * B_BYTE);
+		
+		sample[B1_T][i] = (y & B_1_BITMASK) + 1;
 	}
 	
 	/* sample the remaining part of B_2 for r_t */
-	for (i = B1_REMAINING; i < K1 + 1; i += 4)
+	for (i = B1_REMAINING; i < K1 + 1; i++)
 	{
-		y = _mm256_set_epi64x(LOAD_B(rr + (i + 3) * B_BYTE), LOAD_B(rr + (i + 2) * B_BYTE), LOAD_B(rr + (i + 1) * B_BYTE), LOAD_B(rr + i * B_BYTE));
-		y = _mm256_and_si256(y, V_B_2_BITMASK);
-		y = _mm256_add_epi64(y, V_1_1_1_1);
-			
-		_mm256_storeu_si256((__m256i *)(sample[B1_T] + i), y);
+		y = LOAD_B(rr + i * B_BYTE);
+		
+		sample[B1_T][i] = (y & B_2_BITMASK) + 1;
 	}
 	
 	/* sample those r_i full of B_2 */
 	for (t = B1_T + 1; t < T; t++)
 	{
 		rr = r + B_BYTE * t * (K1 + 1);
-		for (i = 0; i < K1 + 1; i += 4)
+		for (i = 0; i < K1 + 1; i++)
 		{
-			y = _mm256_set_epi64x(LOAD_B(rr + (i + 3) * B_BYTE), LOAD_B(rr + (i + 2) * B_BYTE), LOAD_B(rr + (i + 1) * B_BYTE), LOAD_B(rr + i * B_BYTE));
-			y = _mm256_and_si256(y, V_B_2_BITMASK);
-			y = _mm256_add_epi64(y, V_1_1_1_1);
+			y = LOAD_B(rr + i * B_BYTE);
 			
-			_mm256_storeu_si256((__m256i *)(sample[t] + i), y);
+			sample[t][i] = (y & B_2_BITMASK) + 1;
 		}
 	}
 
@@ -102,26 +91,26 @@ void sampler_zb(uint64_t sample[T][N + K1 + 1])
 		rr = r + (B_BYTE * T * (K1 + 1)) + (t * (K1 + 1) / 8);
 		for (i = 0; i < K1 + 1; i += 8)
 		{
-			x = _mm256_set1_epi64x(rr[i / 8]);
-			x = _mm256_srlv_epi64(x, V_0_1_2_3);
-			z = _mm256_and_si256(x, V_1_1_1_1);
-			z = _mm256_sub_epi64(V_0_0_0_0, z);
-			z = _mm256_and_si256(z, V_fe_fe_fe_fe);
-			z = _mm256_xor_si256(z, V_1_1_1_1);
-			y = _mm256_loadu_si256((__m256i *)(sample[t] + i));
-			y = _mm256_mul_epi32(y, z);
-			y = _mm256_add_epi64(V_Q_Q_Q_Q, y);
-			_mm256_storeu_si256((__m256i *)(sample[t] + i), y);
+			x1 = _mm256_set1_epi64x(rr[i / 8]);
+			x1 = _mm256_srlv_epi64(x1, V_0_1_2_3);
+			z1 = _mm256_and_si256(x1, V_1_1_1_1);
+			z1 = _mm256_sub_epi64(V_0_0_0_0, z1);
+			z1 = _mm256_and_si256(z1, V_fe_fe_fe_fe);
+			z1 = _mm256_xor_si256(z1, V_1_1_1_1);
+			y1 = _mm256_loadu_si256((__m256i *)(sample[t] + i));
+			y1 = _mm256_mul_epi32(y1, z1);
+			y1 = _mm256_add_epi64(V_Q_Q_Q_Q, y1);
+			_mm256_storeu_si256((__m256i *)(sample[t] + i), y1);
 			
-			x = _mm256_srli_epi64(x, 4);
-			z = _mm256_and_si256(x, V_1_1_1_1);
-			z = _mm256_sub_epi64(V_0_0_0_0, z);
-			z = _mm256_and_si256(z, V_fe_fe_fe_fe);
-			z = _mm256_xor_si256(z, V_1_1_1_1);
-			y = _mm256_loadu_si256((__m256i *)(sample[t] + i + 4));
-			y = _mm256_mul_epi32(y, z);
-			y = _mm256_add_epi64(V_Q_Q_Q_Q, y);
-			_mm256_storeu_si256((__m256i *)(sample[t] + i + 4), y);
+			x1 = _mm256_srli_epi64(x1, 4);
+			z1 = _mm256_and_si256(x1, V_1_1_1_1);
+			z1 = _mm256_sub_epi64(V_0_0_0_0, z1);
+			z1 = _mm256_and_si256(z1, V_fe_fe_fe_fe);
+			z1 = _mm256_xor_si256(z1, V_1_1_1_1);
+			y1 = _mm256_loadu_si256((__m256i *)(sample[t] + i + 4));
+			y1 = _mm256_mul_epi32(y1, z1);
+			y1 = _mm256_add_epi64(V_Q_Q_Q_Q, y1);
+			_mm256_storeu_si256((__m256i *)(sample[t] + i + 4), y1);
 		}	
 	}
 }
@@ -168,9 +157,9 @@ void sampler_binomial(uint64_t sample[T][D + K1 + 1])
 {
 	unsigned char r[T * (D + K1 + 1) * BINOMIAL_BYTE];
 	uint32_t i, j;
+	uint32_t x, s;
 	uint32_t t;
 	unsigned char *rr;
-	__m256i x, s, xx, s1, s2;
 	
 	fastrandombytes(r, T * (D + K1 + 1) * BINOMIAL_BYTE);
 	
@@ -181,24 +170,17 @@ void sampler_binomial(uint64_t sample[T][D + K1 + 1])
 		/* x = (r1,r2)
 		 * s1 = bit_sum(r1 & bitmask), s2 = bit_sum(r2 & bitmask), where bit_sum is the number of "1" bits, and bitmask is the mask to select k bits
 		 * sample <-- s1 - s2 */
-		for (i = 0; i < D + K1 + 1; i += 4)
+		for (i = 0; i < D + K1; i++)
 		{
-			x = _mm256_set_epi64x(LOAD_BINOMIAL(rr + (i + 3) * BINOMIAL_BYTE), LOAD_BINOMIAL(rr + (i + 2) * BINOMIAL_BYTE), LOAD_BINOMIAL(rr + (i + 1) * BINOMIAL_BYTE), LOAD_BINOMIAL(rr + i * BINOMIAL_BYTE));
+			x = LOAD_BINOMIAL(rr + i * BINOMIAL_BYTE);
 		
-			s = V_0_0_0_0;
+			s = 0;
 			for (j = 0; j < BINOMIAL_K; j++)
 			{
-				xx = _mm256_srli_epi64(x, j);
-				xx = _mm256_and_si256(xx, V_B_ADDMASK);
-				s = _mm256_add_epi64(s, xx);
+				s += (x >> j) & BINOMIAL_ADDMASK;
 			}
-			
-			s1 = _mm256_and_si256(s, V_B_MASK);
-			s2 = _mm256_srli_epi64(s, BINOMIAL_SHIFT);
-			s = _mm256_add_epi64(V_Q_Q_Q_Q, s1);
-			s = _mm256_sub_epi64(s, s2);
-			
-			_mm256_storeu_si256((__m256i *)(sample[t] + i), s);
+		
+			sample[t][i] = Q + (s & BINOMIAL_MASK) - (s >> BINOMIAL_SHIFT);
 		}
 	}
 }
